f4d00dd665eeb509cbe1f04cb5b6e85410a0fe99
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 20:47:06 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,2 @@
-'''
-Author: Ashley Muka
-Assignment Title: Program 1
-Assignment Description: print Hello World
-Due Date:06/01/2023
-Date Created:06/01/2023
-Date Last Modified:06/01/2023
 
-'''
-
-#input
-#process
-#output
 print("Hello World!")
