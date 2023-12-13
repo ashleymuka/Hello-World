0e39a552af9132201e814022edc0c20205ec3c48
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 20:32:00 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-//Author: Ashley Muka
-//Assignment Title: Program 1
-//Assignment Description: Print Hello World! :-)
-//Due Date: 1/20/2023
-//Date Last Modified: 1/20/2023
-
 
 #include <iostream>
 
