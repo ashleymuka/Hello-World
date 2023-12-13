1396f0f31c14e78daab481acc9562b5d76efcf4f
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 20:30:55 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Author: Ashley Muka
+//Assignment Title: Program 1
+//Assignment Description: Print Hello World! :-)
+//Due Date: 1/20/2023
+//Date Last Modified: 1/20/2023
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << " Hello World!:-) " << endl;
+    return 0;
+}
